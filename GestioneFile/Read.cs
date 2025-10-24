3a96d1203ac13402e6d08942b99ecb657129b226
Alessandro Bianchini <ALESSANDRO.BIANCHINI@local.ittcesena.it>
2025-10-24 09:37:49 +0200

    R0.1:Completata la classe read

--- GestioneFile/Read.cs
+++ GestioneFile/Read.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace GestioneFile
 {
     internal class Read
     {
+        private string filePath;
+
+        public Read(string path)
+        {
+            filePath = path;
+        }
+        public void LeggiFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (Exception e) 
+                {
+                    Console.WriteLine("Errrore nella lettura del file" + e.Message); 
+                }
+        }
     }
 }
